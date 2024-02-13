328b62bdf4a5c434f99ed205e912541a829be9af
bryokn <bryokn@gmail.com>
2024-02-13 15:18:07 +0300

    adding image URLS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h1>Welcome to the Cocktail Bar!</h1>
+      <div className="login-signup">
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
       <h2>Pick your Poison!!</h2>
       <h5>(Not literally!!)</h5>
       <ProductCatalog />
