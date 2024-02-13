846f168ff4276ec511d96e70e664cc0cbcf2439b
bryokn <bryokn@gmail.com>
2024-02-13 14:08:08 +0300

    Partial db.json adding image URLs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import ProductCatalog from "./ProductCatalog";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Welcome to the Cocktail Bar!</h1>
+      <h2>Pick your Poison!!</h2>
+      <h5>(Not literally!!)</h5>
+      <ProductCatalog />
+    </>
+  );
 }
 
 export default App;
