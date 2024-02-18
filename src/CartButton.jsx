aa928a9af416b461c533c34911b97da1960302a8
bryokn <bryokn@gmail.com>
2024-02-18 14:46:24 +0300

    Update Cart button

--- src/CartButton.jsx
+++ src/CartButton.jsx
@@ -27,17 +27,17 @@ function CartButton({ cartCount, selectedProducts, removeFromCart }) {
 
   // Function to remove a single item from the cart
   const handleRemoveFromCart = (index) => {
-    const updatedProducts = [...selectedProducts]; // Create a copy of the selectedProducts array
-    updatedProducts.splice(index, 1); // Remove the item at the specified index
-    removeFromCart(updatedProducts); // Update the cart state with the new array of products
-    calculateTotalPrice(); // Recalculate total price after removing item
+    const updatedProducts = [...selectedProducts]; 
+    updatedProducts.splice(index, 1); 
+    removeFromCart(updatedProducts); 
+    calculateTotalPrice(); 
   };
 
-  // Function to clear the cart
+  //Clear Cart
   const handleClearCart = () => {
-    setTotalPrice(0); // Reset total price to 0
-    onClose(); // Close the modal after clearing the cart
-    removeFromCart([]); // Clear the cart by passing an empty array
+    setTotalPrice(0); 
+    onClose(); 
+    removeFromCart([]);
   };
 
   return (
@@ -48,33 +48,33 @@ function CartButton({ cartCount, selectedProducts, removeFromCart }) {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Your Cart ({cartCount}) Items</ModalHeader>
+          <ModalHeader>Your Cart ({cartCount}) Drinks</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
             <ul>
               {selectedProducts.map((product, index) => (
                 <li key={index}>
-                  <div>{product.name}</div>
+                  <div>{product.name} - {product.brand}</div>
                   <div>${product.price_usd}</div> {/* Update to use correct price property */}
-                  <Button colorScheme="teal" size="sm" onClick={() => handleRemoveFromCart(index)}>Remove</Button>
+                  <Button colorScheme="purple" size="sm" variant="outline" onClick={() => handleRemoveFromCart(index)}>Remove Drink</Button>
                 </li>
               ))}
             </ul>
           </ModalBody>
           <ModalFooter>
-            <Button colorScheme="green" mr={3} onClick={calculateTotalPrice}>
-              Total
+            <Button colorScheme="green" variant="ghost" mr={3} onClick={calculateTotalPrice}>
+              Add Total
             </Button>
             <div>Price: ${totalPrice.toFixed(2)}</div>
-            <Button colorScheme="green" mr={3} onClick={() => {}}>
+            <Button colorScheme="green" variant="outline" mr={3} onClick={() => {}}>
               Pay
             </Button>
             <Button colorScheme="red" onClick={handleClearCart}>
               Clear Cart
             </Button>
-            <Button colorScheme="yellow" mr={3} onClick={onClose}>
+            {/**<Button colorScheme="yellow" mr={3} onClick={onClose}>
               Close
-            </Button>
+            </Button>**/}
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -83,86 +83,3 @@ function CartButton({ cartCount, selectedProducts, removeFromCart }) {
 }
 
 export default CartButton;
-
-
-
-
-
-/** CartButton.jsx
-import React, { useState } from "react";
-import {
-  Button,
-  Badge,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-  useDisclosure,
-} from "@chakra-ui/react";
-
-function CartButton({ cartCount, selectedProducts, clearCart }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  // Function to calculate total price
-  const calculateTotalPrice = () => {
-    let total = 0;
-    selectedProducts.forEach((product) => {
-      total += product.price_usd; // Assuming price_usd is the correct property for the price
-    });
-    setTotalPrice(total);
-  };
-
-  // Function to clear the cart
-  const handleClearCart = () => {
-    clearCart(); // Call the clearCart function passed as prop
-    setTotalPrice(0); // Reset total price to 0
-    onClose(); // Close the modal after clearing the cart
-  };
-
-  return (
-    <>
-      <Button id="cart" colorScheme="teal" variant="solid" onClick={onOpen}>
-        Cart <Badge ml={1} colorScheme="black">{cartCount}</Badge>
-      </Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Your Cart</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <ul>
-              {selectedProducts.map((product, index) => (
-                <li key={index}>
-                  <div>{product.name}</div>
-                  <div>${product.price_usd}</div> {/* Update to use correct price property /}
-                </li>
-              ))}
-            </ul>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="green" mr={3} onClick={calculateTotalPrice}>
-              Total
-            </Button>
-            <div>Price: ${totalPrice.toFixed(2)}</div>
-            <Button colorScheme="green" mr={3} onClick={() => {}}>
-              Pay
-            </Button>
-            <Button colorScheme="red" onClick={handleClearCart}>
-              Clear Cart
-            </Button>
-            <Button colorScheme="yellow" mr={3} onClick={onClose}>
-              Close
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
-}
-
-export default CartButton;
-**/
